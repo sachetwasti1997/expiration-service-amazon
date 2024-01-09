8a7f907b117d9a237df54a45365942e0577e57c5
sachetwasti1997 <sachetwasti61@gmail..com>
2024-01-09 08:21:22 +0530

    first commit

---
+++ src/main/java/com/sachet/expirationserviceamazon/ExpirationServiceAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.sachet.expirationserviceamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpirationServiceAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpirationServiceAmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachet/expirationserviceamazon/ExpirationServiceAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachet.expirationserviceamazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpirationServiceAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
